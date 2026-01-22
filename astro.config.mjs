791a1a9924c04964ee6534aaa7444d6905b8a3a3
LilosG <mprickett@bluedoorsp.com>
2026-01-22 00:37:07 -0800

    Add Flood Cleanup and Sewage Cleanup pillar pages, fix sitemap config and other optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.tamarackrestoration.com',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
-    sitemap({
-      filter: (page) => !page.includes('/404'),
-    }),
   ],
   build: {
     inlineStylesheets: 'auto',
